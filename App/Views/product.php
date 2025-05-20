03ed6e719de1b461c65e497c106c492e7ed0bad1
Nanoargentum <luthanhtuan1a@gmail.com>
2025-05-20 17:04:37 +0700

    5h

--- App/Views/product.php
+++ App/Views/product.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="vi">
-<head>
-  <meta charset="UTF-8">
-  <title>Chi tiết sản phẩm</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <div class="container">
-    <nav class="breadcrumb">
-      Trang chủ / Solid Body Electric Guitar / Fender 70th Anniversary Player...
-    </nav>
+  <head>
+    <meta charset="UTF-8">
+    <title>Chi tiết sản phẩm</title>
+    <link rel="stylesheet" href="style2.css">
+  </head>
+  <body>
+    <div class="container">
+      <nav class="breadcrumb">
+        Trang chủ / Solid Body Electric Guitar / Fender 70th Anniversary Player...
+      </nav>
 
-    <div class="product-detail">
-      <div class="image-gallery">
-        <img class="main-image" src="guitar.png" alt="Guitar">
-        <div class="thumbnails">
-          <img src="thumb1.png" alt="">
-          <img src="thumb2.png" alt="">
-          <img src="thumb3.png" alt="">
-        </div>
-      </div>
-
-      <div class="product-info">
-        <h1>Fender 70th Anniversary Player Stratocaster...</h1>
-        <p class="price">29.040.000₫</p>
-        <p class="installment">hoặc 7.260.000₫/tháng</p>
-        <div class="quantity-cart">
-          <input type="number" value="1" min="1">
-          <button class="add-to-cart">🛒 Thêm vào giỏ hàng</button>
+      <div class="product-detail">
+        <div class="image-gallery">
+          <img class="main-image" src="guitar.png" alt="Guitar">
+          <div class="thumbnails">
+            <img src="thumb1.png" alt="">
+            <img src="thumb2.png" alt="">
+            <img src="thumb3.png" alt="">
+          </div>
         </div>
 
-        <h2>Về Sản Phẩm</h2>
-        <p class="description">
-          Ra mắt bộ sưu tập 70th Anniversary Stratocaster...
-          <br><br>
-          Trải nghiệm phong cách Fender...
-        </p>
+        <div class="product-info">
+          <h1>Fender 70th Anniversary Player Stratocaster...</h1>
+          <p class="price">29.040.000₫</p>
+          <p class="installment">hoặc 7.260.000₫/tháng</p>
+          <div class="quantity-cart">
+            <input type="number" value="1" min="1">
+            <button class="add-to-cart">🛒 Thêm vào giỏ hàng</button>
+          </div>
+
+          <h2>Về Sản Phẩm</h2>
+          <p class="description">
+            Ra mắt bộ sưu tập 70th Anniversary Stratocaster...
+            <br><br>
+            Trải nghiệm phong cách Fender...
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
